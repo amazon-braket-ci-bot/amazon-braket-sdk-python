30113a5fe4a351405cae58258fae3b8b9c895082
ci <ci>
2020-09-03 21:46:37 +0000

    update development version to v1.1.1.dev0

--- src/braket/_sdk/_version.py
+++ src/braket/_sdk/_version.py
@@ -1,4 +1,4 @@
-# Copyright 1.1.0-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 1.1.1.dev0-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
